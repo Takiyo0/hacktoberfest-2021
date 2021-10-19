3401376814ea237dfd182399af815172e6236429
PreethiN0070 <79077181+PreethiN0070@users.noreply.github.com>
2021-10-19 18:37:54 +0530

    updated calculator.c

--- C/calculator.c
+++ C/calculator.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include <string.h>
-
 int main() {
   //var initialization
   char op[9];
@@ -9,9 +8,8 @@ int main() {
   scanf("%s", &op);
   printf("Enter two numbers on which you would like to perform the operation: ");
   scanf("%lf %lf", &n1, &n2);
-
-  if (!strcmp(op, "plus"))
-    printf("%.1lf + %.1lf = %.1lf", n1, n2, n1 + n2);
+if (!strcmp(op, "plus"))
+ printf("%.1lf + %.1lf = %.1lf", n1, n2, n1 + n2);
   else if (!strcmp(op, "minus"))
     printf("%.1lf - %.1lf = %.1lf", n1, n2, n1 - n2);
   else if (!strcmp(op, "multiply"))
@@ -20,6 +18,5 @@ int main() {
     printf("%.1lf / %.1lf = %.1lf", n1, n2, n1 / n2);
   else
     printf("You need to use either of these -  plus , minus multiply , divide");
-
-  return 0;
+return 0;
 }
